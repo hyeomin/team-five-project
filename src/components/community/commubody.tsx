b7e867b71625677a8a197b4f8e18b26f1c6d9552
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2023-12-29 13:59:13 +0900

    feat: 커뮤니티 페이지 WIP

--- src/components/community/commubody.tsx
+++ src/components/community/commubody.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { useRecoilState } from 'recoil';
+import { resolutionType, fetchDataState } from '@/recoil/atom';
+import { Paper } from '@mui/material';
+import MuiCard from './MuiCard';
 
 const Commubody = () => {
-  return <div>Commubody</div>;
+  const [myState, setMyState] = useRecoilState(fetchDataState);
+  console.log('리코일에서 읽어온거', myState);
+
+  return (
+    <>
+      <div className='flex flex-col items-center gap-3'>
+        <h2 className='text-[30px]'>
+          다른 사람들은 어떤 새해 목표를 세웠을까요?
+        </h2>
+        {myState.map((el) => (
+          <MuiCard key={el.id} title={el.title} content={el.content} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Commubody;
