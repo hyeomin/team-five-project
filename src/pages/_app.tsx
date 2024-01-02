fd3bbf85e9088c1c7624f4c376f386c5252bc706
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2024-01-02 20:33:36 +0900

    fix: login 오작동 fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,8 @@ import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { createTheme, colors, ThemeProvider } from '@mui/material';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const theme = createTheme({
   palette: {
@@ -25,6 +27,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <ThemeProvider theme={theme}>
           <Layout>
             <Component {...pageProps} />
+            <ToastContainer />
           </Layout>
         </ThemeProvider>
       </RecoilRoot>
